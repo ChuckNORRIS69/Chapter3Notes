d8e87918942b051da79c3bb05e5675efac34ad05
9ns2 <9ns2@fresnou.org>
2023-04-21 10:30:16 -0700

    initcal commit

--- CharacterCreator.js
+++ CharacterCreator.js
@@ -5,7 +5,7 @@ let character1 = {
     //empty to start
 }
 
-createChar(character1, diceRolled)
+createChar(character1, diceRolled);
 
 function rolld6(){
     return Math.ceil(Math.random()*6);
@@ -14,7 +14,7 @@ function rolld6(){
 function rollDice(amount, list){
     for (let i = 0; i < amount; i++){
         let roll = rolld6();
-        console.log(`You rolled a ${roll}!`)
+        console.log(`You rolled a ${roll}!`);
         list.push(roll)
     }
 }
@@ -48,6 +48,26 @@ function  createChar(character,list) {
     character.wisdom = rollStat(diceRolled);
     console.log("Rolling CHA");
     character.charisma = rollStat(diceRolled);
+    console.log("Rolling CHA");
+    character.fortune = rollStat(diceRolled);
+    console.log("Rolling HIH");
+    character.Height = rollStat(diceRolled);
+    console.log("Rolling age");
+    character.Age = rollStat(diceRolled);
+
+    let answer = readLine.question("Welcome to the great fortune place. Do you want to know your fortune? yes or no: ");
+    while (answer != "yes" && answer != "no"){
+        console.log("invalid choice");
+        answer = readLine.question("Enter y or n: ");
+    }
+    let answer2 = readLine.question("Ask your question: ");
+
+    let answerList = ['It is certain.', 'Without a doubt.', 'My reply is no.', 'My sources say no.', 'Ask again later.', 'Reply hazy, try again'];
+
+    let randomNum = Math.floor(Math.random()*answerList.length);
+
+    let randomAnswer = answerList[randomNum];
+    console.log(randomAnswer);
     console.log(character);
     displayChar(character);
 }
@@ -61,5 +81,27 @@ function displayChar(Char){
     console.log(`INT: ${Char.intelligence}`);
     console.log(`WIS: ${Char.wisdom}`);
     console.log(`CHA: ${Char.charisma}`);
+    console.log(`FOR: ${Char.fortune}`);
+    console.log(`HIH: ${Char.height}`);
+    console.log(`FOR: ${Char.age}`);
     console.log('-----------------------');
 }
+let answer = readLine.question("Welcome to the great fortune place. Do you want to know your fortune? yes or no: ");
+while (answer != "yes" && answer != "no"){
+    console.log("invalid choice");
+    answer = readLine.question("Enter y or n: ");
+}
+
+
+let answer2 = readLine.question("Ask your question: ");
+
+let answer3 = readLine.question("ok see you later:");
+console.log("your welcome any time ");
+let answerList = ['It is certain.', 'Without a doubt.', 'My reply is no.', 'My sources say no.', 'Ask again later.', 'Reply hazy, try again'];
+
+let randomNum = Math.floor(Math.random()*answerList.length);
+
+let randomAnswer = answerList[randomNum];
+console.log(randomAnswer);
+
+
